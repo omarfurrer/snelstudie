0ebda2ef112976c52cf867ced39ba23e98d1d2e6
Omar Furrer <omar.furrer@gmail.com>
2018-11-25 15:47:50 +0200

    Add enums for cities variables and enum trait

---
+++ app/Enums/CitiesVariableDefaultFieldNames.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use App\Traits\Enumerable;
+
+class CitiesVariableDefaultFieldNames {
+
+    use Enumerable;
+
+    const NAME = 'name';
+
+}
---
+++ app/Enums/CitiesVariableDefaultValues.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use App\Traits\Enumerable;
+
+class CitiesVariableDefaultValues {
+
+    use Enumerable;
+
+    const EMPTY_VALUE = 0;
+    const FIXED = 1;
+    const FIELD = 2;
+
+}
