02b5b564b11501124b27c80c5adcac65369279b9
Omar Furrer <omar.furrer@gmail.com>
2018-11-25 16:30:13 +0200

    Change create and update to make sure no dashes in code property

--- app/Repositories/EloquentContentBlocksRepository.php
+++ app/Repositories/EloquentContentBlocksRepository.php
@@ -15,4 +15,31 @@ class EloquentContentBlocksRepository extends EloquentAbstractRepository impleme
         $this->modelClass = ContentBlock::class;
     }
 
+    /**
+     * Create Content Block.
+     * 
+     * @param array $fields
+     * @return ContentBlock
+     */
+    public function create(array $fields = null)
+    {
+        $fields['code'] = str_replace('-', '_', $fields['code']);
+
+        return parent::create($fields);
+    }
+
+    /**
+     * Update Content Block.
+     * 
+     * @param Integer $id
+     * @param array $fields
+     * @return ContentBlock
+     */
+    public function update($id, array $fields = null)
+    {
+        $fields['code'] = str_replace('-', '_', $fields['code']);
+
+        return parent::update($id, $fields);
+    }
+
 }
