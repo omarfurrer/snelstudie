1bac878421a3f48810d3c78e66e4767cb9f17ef9
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-11-26 22:51:06 +0200

    Added home bacground image sector

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    Home
+<div class="homeBgContainer container-fluid">
+	<div class="row text-center homeHeaderRow">
+		<div class="col-md-12">
+			<h1>Translator Translations</h1>
+		</div>
+		<div class="col-md-12">
+			<h4>Professional translations at affordable prices</h4>
+		</div> 
+		<div class="col-md-12">
+			<button class="btn btn-success" id="ctaHomePg">Get A Quotation</button>
+		</div>   
+	</div>
 </div>
+
+
 @endsection
