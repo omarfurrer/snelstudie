be7a223562942fbded4c23903bbf84ba232a2c64
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-12-10 19:51:29 +0200

    Added cards subviewa

---
+++ resources/views/subviews/cards/bigCard.blade.php
@@ -0,0 +1,51 @@
+<div class="container cardHeaderStrip">
+				<div class="row">
+					<div class="col-md-1 text-center sharesDiv">
+						<p class="sharesNo">52</p>
+						<p class="shares">Shares</p>
+					</div>
+					<div class="col-md-1">
+						<a href="#">
+							<img class="avatar" src="../images/avatar.png">
+						</a>
+					</div>
+					<div class="col-md-6 cardHeadTextDiv">
+						<ul class="cardHeadTextUl">
+							<li>
+								<em class="greyText">by </em>
+							</li>
+							<li>
+								<p class="blackText"><a href="#"> Sonia Simone</a></p>
+							</li>
+							<li>
+								<em class="greyText">On </em>
+							</li>
+							<li>
+								<p class="blackText"> December 6, 2018</p>
+							</li>
+						</ul>
+					</div>
+					
+				</div>
+
+				<div class="row">
+					<div class="col-md-9">
+						<a href="#" class="zigzagHeader">
+							A Resource for Managing Holiday Stress While You Strengthen Your Marketing
+						</a>
+
+						<div class="tagsDiv">
+							<a href="#">Books</a>
+							<a href="#">Content Marketing</a>
+							<a href="#">Productivity</a>
+							<a href="#">Selling</a>
+						</div>
+
+						<h4 class="zigzagSubHeader">It’s the start of the traditional winter holidays this week, and you know what that means. Stress, time crunches ……</h4>
+
+						<button class="blueBorderBtn">Continue Reading</button>
+
+					</div>
+				</div>
+
+			</div>
---
+++ resources/views/subviews/cards/card.blade.php
@@ -0,0 +1,36 @@
+<div class="myCard container">
+	<!--  Top Strip -->
+	<div class="row">
+		<div class="col-md-3 text-center sharesDiv">
+			<p class="sharesNo">52</p>
+			<p class="shares">Shares</p>
+		</div>	
+		<div class="col-md-2">
+			<a href="#">
+				<img class="avatar" src="../images/avatar.png">
+			</a>
+		</div>
+		<div class="col-md-2">
+			<p class="cardAuth">
+				<a href="#">
+					Sonia Simone
+				</a>
+			</p>
+		</div>
+	</div>
+	<!--  End of Top Strip -->
+
+	<!-- Card Body -->
+	<div class="row">
+		<div class="col-md-12">
+			<h4 class="smallCardText">3 Steps that Superior Content Creators Can Take to a Badass 2019</h4>
+
+			<!-- Card Tags -->
+			<div class="tagsDiv">
+				<a href="#">Books</a>
+				<a href="#">Productivity</a>
+				<a href="#">Selling</a>
+			</div>
+		</div>	
+	</div>
+</div>	
