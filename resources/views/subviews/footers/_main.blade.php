95331e3b9bd00033673fd01782d4eb50fb08f226
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-12-10 19:55:45 +0200

    Added footer

--- resources/views/subviews/footers/_main.blade.php
+++ resources/views/subviews/footers/_main.blade.php
@@ -1,49 +1,23 @@
-<div class="container-fluid preFooterContainer">
-	<div class="row">
-		<div class="col-md-3">
-			<h5>About us</h5>
-			<p>Translation Agency Scriptware is a team of passionate translators, reviewers, and project managers who help businesses to boost their international communications with perfect translations.</p>
-		</div>
-		<div class="col-md-3">
-			<h5>Instagram</h5>
-			<div class="imgThumbDiv">
-				<img class="thumImg" src="../images/thum1.jpg">
-				<img class="thumImg" src="../images/thum2.jpg">
-				<img class="thumImg" src="../images/thum3.jpg">
-				<img class="thumImg" src="../images/thum4.jpg">
-				<img class="thumImg" src="../images/thum5.jpg">
-				<img class="thumImg" src="../images/thum6.jpg">
-				<img class="thumImg" src="../images/thum7.jpg">
-				<img class="thumImg" src="../images/thum8.jpg">
-				<img class="thumImg" src="../images/thum9.jpg">
+
+<footer class="myFooter">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<p>COPYRIGHT © 2006–2018 RAINMAKER DIGITAL, LLC · POWERED BY THE
+					<a href="">RAINMAKER PLATFORM</a></p>
+				</div>
+			</div>
+			<div class="row mt-2">
+				<div class="col-md-12 text-center">
+					<div class="footerNav">
+					     <a href="">CONTACT</a>
+					     <a href="">COMMENT POLICY</a>
+					     <a href="">PRIVACY POLICY</a>
+					     <a href="">COOKIE POLICY</a>
+					     <a href="">REFUND POLICY</a>
+					     <a href="">TERMS</a>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-3">
-			<h5>Latest news</h5>
-			<a class="latestNewsLink" href="#">About XTM Cloud 10 | Blog </a>
-			<p class="latestNewsP"><strong>17 MAART, 2017</strong></p>
-
-			<a class="latestNewsLink" href="#">About XTM Cloud 10 | Blog </a>
-			<p class="latestNewsP"><strong>17 MAART, 2017</strong></p>
-
-			<a class="latestNewsLink" href="#">About XTM Cloud 10 | Blog </a>
-			<p class="latestNewsP"><strong>17 MAART, 2017</strong></p>
-
-		</div>
-		<div class="col-md-3 text-center">
-			<h3 class="brandHead">Translator</h3>
-			<p class="brandPar">= INNOVATION IN TRANSLATION =</p>
-		</div>
-	</div>
-</div>
-
-<footer class="myfooter">
-	
-	<!-- To the right -->
-	<div class="pull-right hidden-xs">
-		<a href="#" class="footerSmallLinks">NEWS </a>
-		<a href="#" class="footerSmallLinks"> CONTACT</a>
-	</div>
-	<!-- Default to the left -->
-	<p class="footerSmallText">Copyright &copy; 2016 <a href="{{ url('/') }}">{{ config('app.name') }}</a>. All rights reserved.</p> 
-</footer>
+	</footer>
