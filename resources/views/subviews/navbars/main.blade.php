05c9a83929f0b7af94862a8dd96a362d4290aca3
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-12-11 23:48:23 +0200

    Added media queries for ipads and tablets

--- resources/views/subviews/navbars/main.blade.php
+++ resources/views/subviews/navbars/main.blade.php
@@ -7,64 +7,66 @@
           <a class="navbar-brand" href="{{ url('/') }}">
            <img class="navbarLogo" src="../images/logo.png">
          </a>
-         <div class="col-md-3 testCol">
-           <button class="navbar-toggler pull-right myNavTogBtn togBtn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            Menu <span><i class="fa fa-bars"></i></span>
-          </button>
-        </div>
+       </div>
+       <div class="col-md-3 offset-md-6 testCol">
+         <button class="navbar-toggler pull-right myNavTogBtn togBtn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          Menu <span><i class="fa fa-bars"></i></span>
+        </button>
       </div>
+      
+    </div>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav">
 
-        </ul>
+      </ul>
 
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ml-auto">
 
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">Blog <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">Education <span class="sr-only">(current)</span></a>
-          </li> 
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">Certified Writers <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">Services <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">About <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link myNavItem" href="#">My Account <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item noMob">
-            <a class="nav-link myNavItem"  data-toggle="modal" data-target="#exampleModal1" href="#"><i class="fa fa-search"></i> <span class="sr-only">(current)</span></a>
-          </li>
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">Blog <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">Education <span class="sr-only">(current)</span></a>
+        </li> 
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">Certified Writers <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">Services <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">About <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link myNavItem" href="#">My Account <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item noMob">
+          <a class="nav-link myNavItem"  data-toggle="modal" data-target="#exampleModal1" href="#"><i class="fa fa-search"></i> <span class="sr-only">(current)</span></a>
+        </li>
 
-          <li class="nav-item text-center searchMobLi">
-            <form> 
+        <li class="nav-item text-center searchMobLi">
+          <form> 
 
-              <span class="sr-only">(current)</span>
+            <span class="sr-only">(current)</span>
 
-              <input type="search" id="searchMob" name="searchMob" placeholder="Search Copyblogger...">
+            <input type="search" id="searchMob" name="searchMob" placeholder="Search Copyblogger...">
 
-              <input type="submit" id="searchMobCta" value="Search"> 
+            <input type="submit" id="searchMobCta" value="Search"> 
 
-            </form>
-          </li>
-          @guest
+          </form>
+        </li>
+        @guest
 
-          @else
+        @else
 
-          @endguest
-        </ul>
-      </div>
+        @endguest
+      </ul>
     </div>
   </div>
+</div>
 </nav>
 
 <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
