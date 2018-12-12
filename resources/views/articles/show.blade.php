5ff7a2d91a66d444efea66f0d86e0454b09d23e6
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-12-12 16:48:05 +0200

    Added article page and minor fixes

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,87 @@
 @extends('layouts.main')
 
 @section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 text-center mt-5">
+			<h1 class="articleHeroTitle">
+				3 Ways to Persuade People Thinking about Buying from You
+			</h1>
+		</div>
+		<div class="col-md-12 text-center">
+			<div class="articleHeadDiv">
+				
+				<a href="">
+					<img class="articleHeadImg" src="../images/avatar.png">
+				</a>
 
+				<div class="articleHeadItem">
+					<span>
+						<em>Written by:</em>
+					</span>
+					<p><a href="#">Stefanie Flaxman</a></p>
+				</div>
+
+				<div class="articleHeadItem slash">
+					<span>
+						<em>Posted on:</em>
+					</span>
+					<p><a href="#">December 10, 2018</a></p>
+				</div>
+
+				<div class="articleHeadItem articleShares">
+					<span>
+						<em>56</em>
+					</span>
+					<p><a href="#">Shares</a></p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="row mt-5">
+		<div class="col-md-9">
+		</div>
+
+		<div class="col-md-3">
+			<div class="imgBox articleImgBox mt-2">
+				<h4>You need a superior digital marketing and sales education to build a remarkable online presence.</h4>
+				<button class="imgBoxBtn">Get Free Training</button>
+			</div> 
+
+			<div class="articleSideLinks mt-5">
+				<ul>
+					<h4>You may also like:</h4>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a href="">The 7 Things Writers Need to Make a Living</a></li>
+					<li><a class="articleSideLinksLastLink" href="">See All Popular Posts →</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="row mt-5">
+		<div class="col-md-6">
+			<div class="imgBox">
+				<h4>You need a superior digital marketing and sales education to build a remarkable online presence.</h4>
+				<button class="imgBoxBtn">Get Free Training</button>
+			</div> 
+		</div>
+
+		<div class="col-md-6 preFooter">
+			<h1 class="mt-15">Get free access to proven marketing training.</h1>
+		</div>
+
+		<hr class="preFooterHr">
+	</div>
+
+</div>
 
 @endsection
 
